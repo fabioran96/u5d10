c5c7835d6de0032b3fcb69e457dac6fcb526d60f
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-09-13 17:14:29 +0200

    fixed cloudinary

--- src/main/java/fabioran/u5d10/config/ServerConfig.java
+++ src/main/java/fabioran/u5d10/config/ServerConfig.java
@@ -11,9 +11,11 @@ import java.util.Map;
 @Configuration
 public class ServerConfig {
     @Bean
-    public Cloudinary imageUploader(@Value("${cloudinary.name}") String name, @Value("${cloudinary.key}") String key, @Value("${cloudinary.secret}") String secret){
+    public Cloudinary imageUploader(@Value("${CLOUDINARY_NAME}") String name,
+                                    @Value("${CLOUDINARY_KEY}") String key,
+                                    @Value("${CLOUDINARY_SECRET}") String secret){
         Map<String, String> configuration = new HashMap<>();
-        configuration.put("cloud_name",name);
+        configuration.put("cloud_name", name);
         configuration.put("api_key",key);
         configuration.put("api_secret",secret);
         return new Cloudinary(configuration);
