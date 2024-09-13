e6298c1a20f2e42ea871ec1bec7de6a617f11fb3
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-09-13 14:59:04 +0200

    update viaggio e prenotazione services

--- src/main/java/fabioran/u5d10/services/PrenotazioneService.java
+++ src/main/java/fabioran/u5d10/services/PrenotazioneService.java
@@ -1,7 +1,10 @@
 package fabioran.u5d10.services;
 
 import fabioran.u5d10.entities.Prenotazione;
+import fabioran.u5d10.exceptions.NotFoundException;
+import fabioran.u5d10.repositories.DipendenteRepository;
 import fabioran.u5d10.repositories.PrenotazioneRepository;
+import fabioran.u5d10.repositories.ViaggioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,35 @@ import org.springframework.stereotype.Service;
 public class PrenotazioneService {
     @Autowired
     private PrenotazioneRepository prenotazioneRepository;
+    @Autowired
+    private DipendenteRepository dipendenteRepository;
+    @Autowired
+    private ViaggioRepository viaggioRepository;
 
+    //crea prenotazione
     public Prenotazione save(Prenotazione body) {
         return prenotazioneRepository.save(body);
+    }
+
+    //trova prenotazione per id
+    public Prenotazione findById(int id) {
+        return prenotazioneRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Non ho trovato nessuna prenotazione con questo id"));
+    }
+
+    //aggiorna prenotazione
+    public Prenotazione update(int id, Prenotazione body){
+        Prenotazione prenotazione = this.findById(id);
+        prenotazione.setDataRichiesta(body.getDataRichiesta());
+        prenotazione.setNote(body.getNote());
+        return prenotazioneRepository.save(prenotazione);
+    }
+
+
+    //Elimina prenotazione
+
+    public void delete(int id) {
+        Prenotazione found = this.findById(id);
+        prenotazioneRepository.delete(found);
     }
 }
--- src/main/java/fabioran/u5d10/services/ViaggioService.java
+++ src/main/java/fabioran/u5d10/services/ViaggioService.java
@@ -1,6 +1,8 @@
 package fabioran.u5d10.services;
 
+import fabioran.u5d10.entities.StatoViaggio;
 import fabioran.u5d10.entities.Viaggio;
+import fabioran.u5d10.exceptions.NotFoundException;
 import fabioran.u5d10.repositories.ViaggioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,43 @@ public class ViaggioService {
     @Autowired
     private ViaggioRepository viaggioRepository;
 
+    //crea viaggio
     public Viaggio save(Viaggio body){
+        body.setStato(StatoViaggio.IN_PROGRAMMA);
         return viaggioRepository.save(body);
     }
+
+    //findById
+
+    public Viaggio findById(int id) {
+        return viaggioRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Nessun viaggio con questo ID è stato trovato") );
+    }
+
+    //aggiorna viaggio
+
+    public Viaggio update(int id, Viaggio body){
+        Viaggio found = this.findById(id);
+
+        return viaggioRepository.save(found);
+    }
+
+    //aggiorna stato viaggio
+
+    public Viaggio updateStato(int id, StatoViaggio stato){
+        Viaggio found = this.findById(id);
+        found.setStato(stato);
+        return viaggioRepository.save(found);
+    }
+
+    //Elimina viaggio
+
+    public void delete(int id) {
+        Viaggio found = this.findById(id);
+        viaggioRepository.delete(found);
+    }
+
+
+
+
 }
